5ba0a18ebaf889f0ba5fa8dad8dd463b07a3cc3c
HeroFactory16 <cedric.coinlag@gmail.com>
2023-04-21 23:59:03 +0200

    Everything is working

--- links.py
+++ links.py
@@ -1,58 +1,58 @@
 from Spider_soup import sword_master, gunner, bows
 
-sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/tachi.htm", "Long_Sword")
-sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/tachi_g.htm#t8", "Long_Sword")
-sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/tachi_s.htm#t5", "Long_Sword")
+# sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/tachi.htm", "Long_Sword")
+# sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/tachi_g.htm#t8", "Long_Sword")
+# sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/tachi_s.htm#t5", "Long_Sword")
 
-sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/taiken.htm", "Great_Sword")
-sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/taiken_g.htm#t8", "Great_Sword")
-sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/taiken_s.htm#t5", "Great_Sword")
+# sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/taiken.htm", "Great_Sword")
+# sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/taiken_g.htm#t8", "Great_Sword")
+# sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/taiken_s.htm#t5", "Great_Sword")
 
-sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/katate.htm", "Sword_and_Shield")
-sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/katate_g.htm#t8", "Sword_and_Shield")
-sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/katate_s.htm#t5", "Sword_and_Shield")
+# sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/katate.htm", "Sword_and_Shield")
+# sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/katate_g.htm#t8", "Sword_and_Shield")
+# sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/katate_s.htm#t5", "Sword_and_Shield")
 
-sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/souken.htm", "Dual_Swords")
-sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/souken_g.htm#t8", "Dual_Swords")
-sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/souken_s.htm#t5", "Dual_Swords")
+# sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/souken.htm", "Dual_Swords")
+# sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/souken_g.htm#t8", "Dual_Swords")
+# sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/souken_s.htm#t5", "Dual_Swords")
 
-sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/hammer.htm", "Hammer")
-sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/hammer_g.htm#t8", "Hammer")
-sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/hammer_s.htm#t5", "Hammer")
+# sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/hammer.htm", "Hammer")
+# sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/hammer_g.htm#t8", "Hammer")
+# sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/hammer_s.htm#t5", "Hammer")
 
-sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/horn.htm", "Hunting_Horn")
-sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/horn_g.htm#t8", "Hunting_Horn")
-sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/horn_s.htm#t5", "Hunting_Horn")
+# sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/horn.htm", "Hunting_Horn")
+# sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/horn_g.htm#t8", "Hunting_Horn")
+# sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/horn_s.htm#t5", "Hunting_Horn")
 
-sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/lance.htm", "Lance")
-sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/lance_g.htm#t8", "Lance")
-sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/lance_s.htm#t5", "Lance")
+# sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/lance.htm", "Lance")
+# sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/lance_g.htm#t8", "Lance")
+# sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/lance_s.htm#t5", "Lance")
 
-sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/gunlance.htm", "Gunlance")
-sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/gunlance_g.htm#t8", "Gunlance")
-sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/gunlance_s.htm#t5", "Gunlance")
+# sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/gunlance.htm", "Gunlance")
+# sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/gunlance_g.htm#t8", "Gunlance")
+# sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/gunlance_s.htm#t5", "Gunlance")
 
-sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/tonfa.htm", "Tonfa")
-sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/tonfa_g.htm#t8", "Tonfa")
-sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/tonfa_s.htm#t5", "Tonfa")
+# sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/tonfa.htm", "Tonfa")
+# sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/tonfa_g.htm#t8", "Tonfa")
+# sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/tonfa_s.htm#t5", "Tonfa")
 
-sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/slaxe.htm", "Switch_axe")
-sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/slaxe_g.htm#t8", "Switch_axe")
-sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/slaxe_s.htm#t5", "Switch_axe")
+# sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/slaxe.htm", "Switch_axe")
+# sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/slaxe_g.htm#t8", "Switch_axe")
+# sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/slaxe_s.htm#t5", "Switch_axe")
 
-sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/magspike.htm", "Magnet_spike")
-sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/magspike_s.htm#t5", "Magnet_spike")
-
-
+# sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/magspike.htm", "Magnet_spike")
+# sword_master("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/magspike_s.htm#t5", "Magnet_spike")
 
 
-gunner("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/right.htm","Light_Bowgun")
-gunner("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/right_g.htm#t8","Light_Bowgun")
-gunner("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/right_s.htm#t5","Light_Bowgun")
 
-gunner("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/heavy.htm","Heavy_Bowgun")
-gunner("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/heavy_g.htm#t8","Heavy_Bowgun")
-gunner("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/heavy_s.htm#t5","Heavy_Bowgun")
+
+# gunner("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/right.htm","Light_Bowgun")
+# gunner("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/right_g.htm#t8","Light_Bowgun")
+# gunner("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/right_s.htm#t5","Light_Bowgun")
+
+# gunner("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/heavy.htm","Heavy_Bowgun")
+# gunner("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/heavy_g.htm#t8","Heavy_Bowgun")
+# gunner("https://xl3lackout.github.io/MHFZ-Ferias-English-Project/buki/heavy_s.htm#t5","Heavy_Bowgun")
 
 
 
